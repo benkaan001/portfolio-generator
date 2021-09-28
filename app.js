fb636563f92c8c7fc5558389742026b0f47b4bc1
Ben Kaan <benkaan001@gmail.com>
2021-09-28 01:54:45 -0500

    generate webpage via fs

--- app.js
+++ app.js
@@ -1,34 +1,74 @@
-// var commandLineArgs = process.argv;
-// console.log(commandLineArgs);
+// // var commandLineArgs = process.argv;
+// // console.log(commandLineArgs);
+// const fs = require('fs');
 
-const profileDataArgs = process.argv.slice(2, process.argv.lenght);
-console.log(profileDataArgs);
+// const profileDataArgs = process.argv.slice(2, process.argv.lenght);
 
-// const printProfileData = (profileDataArgs) => {
-//     console.log(profileDataArgs);
-// };
+// // const name = profileDataArgs[0];
+// // const github = profileDataArgs[1];
 
-// printProfileData(profileDataArgs);
+// const [name, github] = profileDataArgs;
 
-// const printProfileData = profileDataArr => {
-//     for (let i=0; i<profileDataArr.length; i++){
-//         console.log(profileDataArr[i]);
-//     }
-// };
+// // console.log(profileDataArgs);
 
-const printProfileData = profileDataArr => {
-    for (let i=0; i<profileDataArr.lenght; i+=1){
-        console.log(profileDataArr[i]);
+// // const generatePage = () => 'Name: Jane, Github: janehub';
 
-    }
-    console.log('=================');
+// // console.log(generatePage());
 
-    // profileDataArr.forEach( (profileItem) => {
-    //     console.log(profileItem);
-    // )};
+// // const generatePage = (userName, githubName) => `Name: ${userName}, Github: ${githubName}`;
+// // console.log(generatePage('Jane','janehub'));
 
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-    
-};
+// // const generatePage = (userName, githubName) =>{
+// //     return `
+// //     Name: ${userName}
+// //     Github: ${githubName}
+// //     `;
+// // };
 
-printProfileData(profileDataArgs);
+
+// const generatePage = (name, github) => {
+//     return `
+//     <!DOCTYPE html> 
+//     <html lang="en"> 
+//     <head>
+//       <meta charset="UTF-8">
+//       <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//       <meta http-equiv="X-UA-Compatible" content="ie=edge">
+//       <title>Portfolio Demo</title>
+//     </head>
+  
+//     <body>
+//       <h1>${name}</h1>
+//       <h2><a href="https://github.com/${github}">Github</a></h2>
+//     </body>
+//     </html>
+//     `;
+//   };
+
+// //   console.log (name, github);
+// // console.log(generatePage(name, github));
+
+// fs.writeFile('index.html', generatePage(name, github), err =>{
+//     if(err) throw err;
+
+//     console.log('Portfolio complete! Check out index.html to see the output!');
+// });
+
+
+// const { generatePrime } = require('crypto');
+const fs = require('fs');
+
+const generatePage = require('./src/page-template.js');
+
+const profileDataArgs = process.argv.slice(2);
+
+const [name, github] = profileDataArgs;
+
+
+
+fs.writeFile('./index.html', generatePage(name, github), err => {
+    if (err) throw new Error(err);
+
+    console.log('Portfolio complete! Check out index.html to see the output!');
+});
+
